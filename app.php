b922094bb5c2be741a24611045085f6a4fcc6abf
Bonjardim <wellingtonbonjardim7@gmail.com>
2022-10-05 20:00:14 -0300

    Criando as classes DASHBOARD, CONEXAO e BD

---
+++ app.php
@@ -0,0 +1,61 @@
+<?php
+    //classe dashboard
+    class Dashboard {
+        public $data_inicio;
+        public $data_fim;
+        public $numeroVendas;
+        public $totalVendas;
+
+        public function __get($atributo) {
+            return $this->$atributo;
+        }
+
+        public function __set($atributo, $valor) {
+            $this->$atributo = $valor;
+            return $this;
+        }
+    }
+
+    //classe de conexão com o banco
+    class Conexao {
+        private $host = 'localhost';
+        private $dbname = 'dashboard';
+        private $user = 'root';
+        private $pass = '';
+
+        public function conectar() {
+            try {
+                $conexao = new PDO(
+                    "mysql:host=$this->host; dbname=$this->dbname",
+                    "$this->user",
+                    "$this->pass"
+                );
+
+                $conexao->exec('set charset utf8');
+
+                return $conexao;
+                
+            } catch(PDOException $e) {
+                echo '<p>'. $e->getMessage() .'</p>';
+            }
+        }
+    }
+
+    //classe model
+    class Bd {
+        private $conexao;
+        private $dashboard;
+
+        public function __construct(Conexao $conexao, Dashboard $dashboard) {
+            $this->conexao = $conexao->conectar();
+            $this->dashboard = $dashboard;
+        }
+    }
+
+    $dashboard = new Dashboard();
+
+    $conexao = new Conexao();
+
+    $bd = new Bd($conexao, $dashboard);
+
+?>
